4e13fc4ff1f4e41b2b2133abdba7f93352e7f266
Mac <mac_tilma@outlook.com>
2020-03-04 21:05:00 +0000

    trying a one page app layout

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="grid-container">
+        <div class="grid-item">1</div>
+        <div class="grid-item">2</div>
+        <div class="grid-item">3</div>
+        <div class="grid-item">4</div>
+    </div>
+@endsection
