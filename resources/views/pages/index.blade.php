1fe3768ac6f63a2b5a2217315ca5e13c93cdfdfc
Mac <mac_tilma@outlook.com>
2020-03-04 21:50:02 +0000

    playing around with flexbox and partials

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,24 +1,50 @@
 @extends('layouts.app')
     <style>
-        .grid-container {
-            display: grid;
-            grid-template-columns: auto auto auto;
-            background-color: #2196F3;
-            padding: 10px;
+
+        .row {
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            width: 100%;
         }
-        .grid-item {
-            background-color: rgba(255, 255, 255, 0.8);
-            border: 1px solid rgba(0, 0, 0, 0.8);
-            padding: 20px;
-            font-size: 30px;
-            text-align: center;
+
+        .column {
+            display: flex;
+            flex-direction: column;
+            flex-basis: 100%;
+            flex: 1;
         }
+
+        .left-table {
+            background-color: gray;
+            height: 700px;
+        }
+
+        .map-div {
+            background-color: lightblue;
+            height: 700px;
+        }
+        .double-column {
+            display: flex;
+            flex-direction: column;
+            background-color: pink;
+            flex-basis: 100%;
+            flex: 2;
+        }
+
     </style>
 @section('content')
-    <div class="grid-container">
-        <div class="grid-item">1</div>
-        <div class="grid-item">2</div>
-        <div class="grid-item">3</div>
-        <div class="grid-item">4</div>
-    </div>
+        <div class="row">
+            <div class="column">
+                <div class="left-table">1 Here is where we show car mileage</div>
+            </div>
+            <div class="column">
+                <div class="map-div">2 here is where we display the map</div>
+            </div>
+        </div>
+        <div class="row 2">
+            <div class="double-column">
+                <div class="bottom-table">3 here is where we display extra info</div>
+            </div>
+        </div>
 @endsection
