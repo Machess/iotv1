26b52b70aea382f260b62b1be998e95af39333a5
Mac <mac_tilma@outlook.com>
2020-03-05 18:03:41 +0000

    Added map and table

--- resources/views/layouts/partial/map-div.blade.php
+++ resources/views/layouts/partial/map-div.blade.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mac_t
- * Date: 3/4/2020
- * Time: 9:16 PM
- */
+<style>
+    .column {
+        display: flex;
+        flex-direction: column;
+        flex-basis: 100%;
+        flex: 1;
+    }
+    #mapid {
+        height: 1000px;
+        width: 1000px;
+    }
+</style>
+<div class="column">
+    <div id="mapid"></div>
+</div>
+
+
+
+
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,50 +1,5 @@
 @extends('layouts.app')
-    <style>
-
-        .row {
-            display: flex;
-            flex-direction: row;
-            flex-wrap: wrap;
-            width: 100%;
-        }
-
-        .column {
-            display: flex;
-            flex-direction: column;
-            flex-basis: 100%;
-            flex: 1;
-        }
-
-        .left-table {
-            background-color: gray;
-            height: 700px;
-        }
-
-        .map-div {
-            background-color: lightblue;
-            height: 700px;
-        }
-        .double-column {
-            display: flex;
-            flex-direction: column;
-            background-color: pink;
-            flex-basis: 100%;
-            flex: 2;
-        }
-
-    </style>
 @section('content')
-        <div class="row">
-            <div class="column">
-                <div class="left-table">1 Here is where we show car mileage</div>
-            </div>
-            <div class="column">
-                <div class="map-div">2 here is where we display the map</div>
-            </div>
-        </div>
-        <div class="row 2">
-            <div class="double-column">
-                <div class="bottom-table">3 here is where we display extra info</div>
-            </div>
-        </div>
+    @include('layouts.partial.left-table')
+    @include('layouts.partial.map-div')
 @endsection
