e145ec16a9abfce0c7d94ab12f7838ac2d5b99f0
Mac <mac_tilma@outlook.com>
2020-03-16 22:03:45 +0000

    table layout fixes in fleet management

--- resources/views/layouts/partial/bottom-table-fleet.blade.php
+++ resources/views/layouts/partial/bottom-table-fleet.blade.php
@@ -1,14 +1,14 @@
 <div style="width: 100%; float:left">
     <div id="table-bottom-toggle" style="width: 50%; margin: 2%">
         <table class="table">
-            <thead>
-            <tr>
-                <th>Type</th>
-                <th>Brand</th>
-            </tr>
-            </thead>
-            <tbody>
             @if(count($fleets) > 0)
+                <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Brand</th>
+                </tr>
+                </thead>
+                <tbody>
                 @foreach($fleets as $fleet)
                     <tr>
                         <td>{{$fleet->type}}</td>
@@ -21,10 +21,10 @@
                     </tr>
                 @endforeach
             @else
-                <p> No assigned fleet</p>
+                <p class="text-danger"> No assigned fleet</p>
             @endif
             </tbody>
         </table>
     </div>
-    <a href="/fleets/create" class="btn btn-primary"> Add more to fleet</a>
+    <a href="/fleets/create" class="btn btn-primary" style="margin-left: 25px"> Add more to fleet</a>
 </div>
